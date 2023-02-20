2d3f37608f086c97c12af5d2a58ec0985184aec1
pabarcag <patricioabarca88@gmail.com>
2023-02-20 19:23:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# Frontend Mentor - Interactive pricing component
+
+![Design preview for the Interactive pricing component coding challenge](./design/desktop-preview.jpg)
+
+## Welcome! 👋
+
+Thanks for checking out this front-end coding challenge.
+
+[Frontend Mentor](https://www.frontendmentor.io) challenges help you improve your coding skills by building realistic projects.
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+## The challenge
+
+Your challenge is to build out this interactive pricing component and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Use the slider and toggle to see prices for different page view numbers (details provided below)
+
+Want some support on the challenge? [Join our Slack community](https://www.frontendmentor.io/slack) and ask questions in the **#help** channel.
+
+### Page view and pricing totals
+
+Here are the different page view ranges and the corresponding monthly price totals:
+
+- 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
+
+If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+
+### Links
+
+- Solution URL: [Github code](https://github.com/pabarcag/ping-simple-page)
+- Live Site URL: [Live site](https://pabarcag.github.io/ping-simple-page/)
+
+
+## My process
+
+### Built with
+
+- Mobile-first workflow
+- Semantic HTML5 markup
+- CSS custom properties
+- BEM
+- Flexbox
+- GRID
+- SASS
+- JavaScript
+
+
+### What I learned
+
+I learned a lot in this challenge. Starting with CSS, I learned how to customize range input, since I had no idea that was possible, I also learned how to do layout effects with grid (change element positions which within the HTML flow is impossible), I learned how to create a simple switch. Enjoy doing the CSS.
+
+Now with Javascript, I feel that it is not the most optimal solution that I did. But, being the second challenge I do, it's not bad at all.
+Here I learned a little about event delegation, control input type events, a simple switch validation.
+Although I'm not satisfied with what I did in logic, I have to keep learning!
+
+
+## Author
+- Frontend Mentor - [@patosinsuerte](https://www.frontendmentor.io/profile/Patosinsuerte)
---
+++ js/index.js
@@ -0,0 +1,90 @@
+const $range = document.querySelector('.form__range');
+const $priceText = document.querySelector('.form__price');
+const $pageviewsText = document.querySelector('.form__pagetext');
+const $form = document.getElementById('form');
+const $switchBg = document.querySelector('.form__switch');
+const $switchCircle = document.querySelector('.form__circle');
+const $submitButton = document.querySelector('.form__submit');
+const prices = [8,12,16,24,36];
+const pageTexts = ['10K pageviews','50K pageviews','100K pageview','500k pageviews','1M pageviews'];
+
+
+function filterNum (array,number){
+    const numFilter = array.filter((element)=>{ return element == number});
+    return numFilter
+ }
+
+
+ function filterText (array,word){
+     const textFilter = array.filter((element)=>element == word);
+     return textFilter;
+ }
+
+ function showPrice(){
+    if(range.value == 1){
+        $priceText.textContent = '$'+filterNum(prices, 8) +'.00';
+        $pageviewsText.textContent = filterText(pageTexts, '10K pageviews')
+    }
+    if(range.value == 2){
+        $priceText.textContent = '$'+filterNum(prices, 12) +'.00';
+        $pageviewsText.textContent = filterText(pageTexts, '50K pageviews')
+    }
+    if(range.value == 3){
+        $priceText.textContent = '$'+filterNum(prices, 16) +'.00';
+        $pageviewsText.textContent = filterText(pageTexts, '100K pageview')
+    }
+    if(range.value == 4){
+        $priceText.textContent = '$'+filterNum(prices, 24) +'.00';
+        $pageviewsText.textContent = filterText(pageTexts, '500k pageviews')
+    }
+    if(range.value == 5){
+        $priceText.textContent = '$'+filterNum(prices, 36) +'.00';
+        $pageviewsText.textContent = filterText(pageTexts, '1M pageviews')
+    }
+}
+
+function calcDiscount(){
+    if(switchActive == true && range.value == 1){
+        $priceText.textContent = '$'+filterNum(prices, 8)/100*75 +'.00';
+    }
+    if(switchActive == true && range.value == 2){
+        $priceText.textContent = '$'+ Math.round(filterNum(prices, 12)/100*75) +'.00';
+    }
+    if(switchActive == true && range.value == 3){
+        $priceText.textContent = '$'+ filterNum(prices, 16)/100*75 +'.00';
+    }
+    if(switchActive == true && range.value == 4){
+        $priceText.textContent = '$'+ filterNum(prices, 24)/100*75 +'.00';
+    }
+    if(switchActive == true && range.value == 5){
+        $priceText.textContent = '$'+ filterNum(prices, 36)/100*75 +'.00';
+    }
+}
+
+
+/*switch*/
+ let switchActive = false;
+
+ document.addEventListener('click',(e)=>{
+     if(e.target.matches('.form__switch') || e.target.matches('.form__circle')){
+         $switchBg.classList.toggle('form__switch--active');
+         $switchCircle.classList.toggle('form__circle--active');
+         switchActive = !switchActive;
+         if(switchActive == true){
+            calcDiscount();
+         }else{
+            showPrice()
+         }
+
+     }
+ 
+ });
+
+ /*range*/ 
+$range.addEventListener('change',(e)=>{
+    showPrice();
+        if(switchActive == true){
+            calcDiscount();
+         }
+});
+
